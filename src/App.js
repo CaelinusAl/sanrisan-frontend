85249a80b65f8de0e16132a30197ad713bc74980
Selin Irmak <selin.irmak89@gmail.com>
2026-01-27 18:49:32 +0300

    fix: correct Navbar and Footer import paths

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 ﻿import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function Home() {
+import { Navbar } from './components/layout/Navbar';function Home() {
   return <div>Ask Sanrı Home</div>;
 }
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+  <Navbar />
+  <Routes>...</Routes>
+</BrowserRouter>
   );
 }
