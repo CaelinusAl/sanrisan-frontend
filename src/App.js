5f287847ba6fa2b062229ebb846e5f0ba715c807
Selin Irmak <selin.irmak89@gmail.com>
2026-01-29 17:19:34 +0300

    feat: connect AskSanriPage to home route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 ﻿import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Footer } from "./components/layout/Footer";
-import docs from "./data/docs.json";
 import AskSanriPage from "./pages/AskSanriPage";
-
-<Route path="/ask" element={<AskSanriPage />} />
-function Home() {
-  const { title, subtitle, status } = docs.home;
-  return (
-    <div style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1 style={{ margin: 0 }}>{title}</h1>
-      <p style={{ marginTop: 8 }}>{subtitle}</p>
-      <p style={{ marginTop: 8, opacity: 0.8 }}>{status}</p>
-    </div>
-  );
-}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/ask" element={<AskSanriPage />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<AskSanriPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
