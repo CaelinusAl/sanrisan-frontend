fc69f1d87e4154ebc3c6c5eea16d96f61b320ae1
Selin Irmak <selin.irmak89@gmail.com>
2026-01-29 23:44:36 +0300

    fix: correct AskSanriPage import case for vercel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-﻿import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Footer } from "./components/layout/Footer";
-import AskSanriPage from "./pages/AskSanriPage";
-
-export default function App() {
+﻿import AskSanriPage from "./pages/ask-sanri-page";
+export default function AskSanriPage() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AskSanriPage />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <div style={{ padding: 32, fontFamily: "system-ui" }}>
+      <h1>ASK SANRI</h1>
+      <p>Bilinçle sor, bilinçle cevap al.</p>
+
+      <div style={{ marginTop: 24, opacity: 0.8 }}>
+        <p>Deploy başarılı.</p>
+        <p>Şimdi modüller tek tek aktif ediliyor.</p>
+      </div>
+    </div>
   );
 }
